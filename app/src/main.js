24b6f9e51a6b1d0378abc8f590013de6a868cae1
anusorn <anusorn.rochana-aree@tr.com>
2016-10-21 19:02:31 +0700

    added react tags + use bower to manage dependencies

--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,4 @@
+
 ReactDOM.render(
 <h1>Tech Stacks</h1>,
 document.getElementById('example')
@@ -5,16 +6,16 @@ document.getElementById('example')
 
 
 var data = [
-  {id: 1, name: "EK", skills: "c++,c#,javascript"},
-  {id: 2, name: "FT", skills: "c#,wpf"},
-  {id: 3, name: "AS", skills: "java,javascript,c#,go"}
+  {id: 1, name: "EK", skills: ["c++","c#","javascript"]},
+  {id: 2, name: "FT", skills: ["c#","wpf"]},
+  {id: 3, name: "AS", skills: ["java","javascript","c#","go"]}
 ];
 
 var TeamBox = React.createClass({
   render: function() {
     var teamNodes = data.map(function(teamInfo) {
       return (
-        <div>
+        <div key={teamInfo.id}>
           {teamInfo.name} : <TeamSkillTags skills={teamInfo.skills}/>
         </div>
       );
@@ -31,11 +32,26 @@ var TeamSkillTags = React.createClass({
   render: function() {
     return (
       <span className="teamSkillTags">
-        <textarea value={this.props.skills}/>
+        <Tags data={this.props.skills}/>
       </span>
     );
   }
 });
+
+class Tags extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {tags: props.data}
+  }
+ 
+  handleChange(tags) {
+    this.setState({tags});
+  }
+ 
+  render() {
+    return <ReactTagsInput value={this.state.tags} onChange={this.handleChange.bind(this)} inputProps={{placeholder:"Add Tech"}}/>
+  }
+}
 
 ReactDOM.render(
   <TeamBox />,
