e6fc589af1541f26a553d20a1f5aa07fc3aa4454
Ekawat S <ekawat.srisattayasunthorn@thomsonreuters.com>
2016-11-11 19:11:08 +0700

    Apply fetch data from aws

--- app/src/main.js
+++ app/src/main.js
@@ -1,17 +1,10 @@
 
 ReactDOM.render(
-<h1>Tech Stacks</h1>,
-document.getElementById('example')
+  <h1>Tech Stacks</h1>,
+  document.getElementById('example')
 );
 
-
-var data = [
-  {id: 1, name: "EK", skills: ["c++","c#","javascript"]},
-  {id: 2, name: "FT", skills: ["c#","wpf"]},
-  {id: 3, name: "AS", skills: ["java","javascript","c#","go"]}
-];
-
-var getRequest = new Request('https://pbntn752kg.execute-api.us-west-2.amazonaws.com/prod/TechStacksMicroService?TableName=TeamTable', {
+var getTeamRequest = new Request('https://ax2nrqdj3b.execute-api.us-west-2.amazonaws.com/prod/techstack?TableName=TeamTable', {
     method: "GET",
     mode: 'cors',
     headers: {
@@ -20,51 +13,35 @@ var getRequest = new Request('https://pbntn752kg.execute-api.us-west-2.amazonaws
     }
 });
 
-fetch(getRequest)  
-  .then(  
-    function(response) {  
-      if (response.status !== 200) {  
-        console.log('Looks like there was a problem. Status Code: ' +  
-          response.status);
-        return;  
-      }
+class TeamBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: [{TeamId: 0, TeamName: "", Skills: ["Loading..."]}]}
+  }
 
-      // Examine the text in the response  
-      response.json().then(function(data) {  
-        console.log(data);  
-      });  
-    }  
-  )  
-  .catch(function(err) {  
-    console.log('Fetch Error :-S', err);  
-  });
-
-var TeamBox = React.createClass({
-  render: function() {
-    var teamNodes = data.map(function(teamInfo) {
-      return (
-        <div key={teamInfo.id}>
-          {teamInfo.name} : <TeamSkillTags skills={teamInfo.skills}/>
-        </div>
-      );
+  componentDidMount() {
+    fetch(getTeamRequest).then((response) => {
+      var teamBox = this;
+      response.json().then((json) => {
+        teamBox.setState({data: json.Items});
+      }); 
     });
-    return (
-      <div className="teamBoxes">
-        {teamNodes}
-      </div>
-    );
   }
-});
 
-var TeamSkillTags = React.createClass({
-  render: function() {
-    return (
-      <span className="teamSkillTags">
-        <Tags data={this.props.skills}/>
-      </span>
-    );
+  render() {
+      var teamNodes = this.state.data.map(function(teamInfo) {
+        return <div key={teamInfo.TeamId}>{teamInfo.TeamName} : <TeamSkillTags skills={teamInfo.Skills}/></div>;
+      });
+
+      return <div className="teamBoxes">{teamNodes}</div>;
   }
-});
+}
+
+class TeamSkillTags extends React.Component {
+  render() {
+    return <span className="teamSkillTags"><Tags data={this.props.skills}/></span>;
+  }
+}
 
 class Tags extends React.Component {
   constructor(props) {
@@ -82,6 +59,6 @@ class Tags extends React.Component {
 }
 
 ReactDOM.render(
-  <TeamBox />,
+  <TeamBox/>,
   document.getElementById('content')
 );
