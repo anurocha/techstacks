600e5271d80a642219634304dcc0ea66cbd4dd5d
anusorn.rochana-aree <anusorn.rochana-aree@thomsonreuters.com>
2016-12-16 18:10:44 +0700

    rebased the code remove duplicated code

--- app/src/datasvc.js
+++ app/src/datasvc.js
@@ -1,32 +1,41 @@
  class DataService {
-     constructor(){
-         console.log('data service has been craeted...')
-     }
+    constructor(){
+        console.log('data service has been craeted...')
+    }
      
-     getTeamData( callback ){
-        var self = this;
-        var getRequest = new Request('https://gtyhwdigg9.execute-api.us-west-2.amazonaws.com/prod/TechStacksMicroService?TableName=TSteam', {
-            method: "GET",
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Api-Key' : 'AXdaESDbtV6d0uqFZ8mFWaCs2axVDI7a5MujwZm7'
-            }
-        });
+    getTeamData( callback ){
+        this.requestTo('https://gtyhwdigg9.execute-api.us-west-2.amazonaws.com/prod/TechStacksMicroService?TableName=TSteam', callback);
+    }
 
-        fetch(getRequest)  
-            .then((response) => {  
-                if (response.status !== 200) {  
-                    console.log('Looks like there was a problem. Status Code: ' +  response.status);
-                    return;  
-                }
-                response.json().then( (data)=> {
-                    callback(data.Items);
-                //self.setState({teamData});
-                });  
-            })  
-            .catch(function(err) {  
-                console.log('Fetch Error :-S', err);  
-            });
+    getSummaryData( callback ) {
+      this.requestTo('https://gtyhwdigg9.execute-api.us-west-2.amazonaws.com/prod/TechStacksMicroService?TableName=TSteam&Summary=true', callback);
+    }
+
+    requestTo(url, callback){
+      var self = this;
+      var getRequest = new Request(url, {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Api-Key' : 'AXdaESDbtV6d0uqFZ8mFWaCs2axVDI7a5MujwZm7'
         }
+      });
+
+      fetch(getRequest)  
+        .then((response) => {  
+            if (response.status !== 200) {  
+              console.log('Looks like there was a problem. Status Code: ' +  
+                response.status);
+              return;  
+            }
+            response.json().then( (data)=> {
+              callback(data.Items);
+            });  
+          }  
+        )  
+        .catch(function(err) {  
+          console.log('Fetch Error :-S', err);  
+        });
+    }
 }
